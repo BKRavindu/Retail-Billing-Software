c9c675f727fc84adbbb9ffa6e82c18acfac5ec7b
BKRavindu <bkravindu2000@gmail.com>
2025-08-09 00:58:23 +0530

    Database connection and frontend setup

---
+++ backend/src/main/java/com/pm/backend/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package com.pm.backend.controller;
+
+public class CategoryController {
+}
---
+++ backend/src/main/java/com/pm/backend/entity/CategoryEntity.java
@@ -0,0 +1,35 @@
+package com.pm.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbl_category")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String categoryId;
+    @Column(unique = true)
+    private String name;
+    private String description;
+    private String imageUrl;
+    private String bgColor;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
