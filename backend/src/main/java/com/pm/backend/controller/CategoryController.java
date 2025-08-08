14932247d42570bb1b8d0d8e3f47bf9dd11df633
BKRavindu <bkravindu2000@gmail.com>
2025-08-09 02:23:16 +0530

    Category creation API created

--- backend/src/main/java/com/pm/backend/controller/CategoryController.java
+++ backend/src/main/java/com/pm/backend/controller/CategoryController.java
@@ -1,4 +1,23 @@
 package com.pm.backend.controller;
 
+import com.pm.backend.Service.CategoryService;
+import com.pm.backend.io.CategoryRequest;
+import com.pm.backend.io.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponse addCategory(@RequestBody CategoryRequest request) {
+        return categoryService.add(request);
+
+    }
 }
--- backend/src/main/java/com/pm/backend/io/CategoryRequest.java
+++ backend/src/main/java/com/pm/backend/io/CategoryRequest.java
@@ -1,4 +1,13 @@
 package com.pm.backend.io;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class CategoryRequest {
+    private String name;
+    private String description;
+    private String bgColor;
+
 }
